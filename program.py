940b451b28997533a8403d3f63f8beeb97736d21
ved1beta <vedantthote2019@gmail.com>
2025-01-11 07:46:42 +0530

    writing attention in triton

--- program.py
+++ program.py
@@ -2,6 +2,41 @@ import torch
 import triton
 import triton.language as tl 
 
+@triton.jit
+def _attn_fwd(
+    Q,  # BATCH_SIZE, NUM_HEADS, SEQ_LEN, HEAD_DIM
+    K,  # BATCH_SIZE, NUM_HEADS, SEQ_LEN, HEAD_DIM
+    V,  # BATCH_SIZE, NUM_HEADS, SEQ_LEN, HEAD_DIM
+    softmax_scale,
+    M,  # BATCH_SIZE, NUM_HEADS, SEQ_LEN
+    O,  # BATCH_SIZE, NUM_HEADS, SEQ_LEN, HEAD_DIM
+    stride_Q_batch,
+    stride_Q_head,
+    stride_Q_seq,
+    stride_Q_dim,
+    stride_K_batch,
+    stride_K_head,
+    stride_K_seq,
+    stride_K_dim,
+    stride_V_batch,
+    stride_V_head,
+    stride_V_seq,
+    stride_V_dim,
+    stride_O_batch,
+    stride_O_head,
+    stride_O_seq,
+    stride_O_dim,
+    BATCH_SIZE,
+    NUM_HEADS: tl.constexpr,
+    SEQ_LEN: tl.constexpr,
+    HEAD_DIM: tl.constexpr,
+    BLOCK_SIZE_Q: tl.constexpr,
+    BLOCK_SIZE_KV: tl.constexpr,
+    STAGE: tl.constexpr,
+):
+    tl.static_assert(BLOCK_SIZE_KV <= HEAD_DIM)
+
+    
 class TritonAttention(torch.autograd.Function):
     @staticmethod
     def forward(ctx, Q, K, V, casual , softmax_scale ):
@@ -14,13 +49,52 @@ class TritonAttention(torch.autograd.Function):
         O = torch.empty_like(Q)
 
         stage = 3 if casual else 1 
-        grind = lambda agrs:(
+        grid = lambda agrs:(
 
         torch.cdiv(SEQ_LEN, agrs["BLOCK_SIZE_Q"]), 
         BATCH_SIZE*NUM_HEADS,
         1
-
         )
+        M = torch.empty(
+        (BATCH_SIZE, NUM_HEADS ,SEQ_LEN), device= Q.device, dtype=torch.float32
+        )
+
+        _attn_fwd[grid](
+            Q=Q,
+            K=K,
+            V=V,
+            softmax_scale=softmax_scale,
+            M=M,
+            O=O,
+            stride_Q_batch=Q.stride(0),
+            stride_Q_head=Q.stride(1),
+            stride_Q_seq=Q.stride(2),
+            stride_Q_dim=Q.stride(3),
+            stride_K_batch=K.stride(0),
+            stride_K_head=K.stride(1),
+            stride_K_seq=K.stride(2),
+            stride_K_dim=K.stride(3),
+            stride_V_batch=V.stride(0),
+            stride_V_head=V.stride(1),
+            stride_V_seq=V.stride(2),
+            stride_V_dim=V.stride(3),
+            stride_O_batch=O.stride(0),
+            stride_O_head=O.stride(1),
+            stride_O_seq=O.stride(2),
+            stride_O_dim=O.stride(3),
+            BATCH_SIZE=Q.shape[0],
+            NUM_HEADS=Q.shape[1],
+            SEQ_LEN=Q.shape[2],
+            HEAD_DIM=HEAD_DIM_K,
+            STAGE=stage,
+        )
+
+        ctx.save_for_backward(Q, K, V, O, M)
+        ctx.grid = grid
+        ctx.softmax_scale = softmax_scale
+        ctx.HEAD_DIM = HEAD_DIM_K
+        ctx.causal = casual
+        return O
 
 
 
